45d6507ed891c7d4b963f2f3cd176867316d2c68
hues4789 <yosikawashuhei0225@gmail.com>
2020-06-23 22:18:01 +0900

    登録画面遷移完了

---
+++ Memory/app/src/main/java/com/example/memory/taskCreate.java
@@ -0,0 +1,13 @@
+package com.example.memory;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class taskCreate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_create);
+    }
+}
